4b0a7aa1575a272420779d6da47c8c8352033267
Ivan Filimonov <snake73w@gmail.com>
2017-01-13 23:21:21 +0700

    add form handler

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from typograph import typograph
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def form():
-    return render_template('form.html')
+    input_text = request.form['text'] if request.method == 'POST' else ''
+    formatted_text = typograph(input_text)
+    return render_template('form.html',
+                           input_text=input_text,
+                           formatted_text=formatted_text)
+
 
 if __name__ == "__main__":
     app.run()
