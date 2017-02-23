fe2a5cee58ff4435eddccb4c456288b1141deb50
Ivan Filimonov <snake73w@gmail.com>
2017-02-24 00:07:27 +0700

    put "templates" to module constants

--- typograph.py
+++ typograph.py
@@ -1,6 +1,26 @@
 import re
 
 
+PATTERNS = [
+    # remove windows line breaks
+    (r'\n\r|\r\n', r'\n'),
+    # remove redundant spaces and line breaks
+    (r'([\s])\1+', r'\1'),
+    # remove redundant line breaks at start of line
+    (r'^[\n ]+([^\n ])', r'\1'),
+    # replace quotes
+    (r"""(['"])(.*)(\1)""", r'«\2»'),
+    # replace hyphen on dash after punctuation
+    (r'(\.{3}\s*|[!?\.,]\s*|^\s*)(-)', r'\1—'),
+    # replace hyphen on dash between two words
+    (r'([\w]\s+)(-)(\s+[\w])', r'\1—\3'),
+    # binding of numbers followed by the words non-breaking space
+    (r'(\d)([^\w]|_)+([^\W\d_])', r'\1&nbsp;\3'),
+    # binding short words followed by the words
+    (r'(\b[^\W\d]{1,2})\s+\b', r'\1&nbsp;'),
+]
+
+
 def replace_hyphen_on_shortdash_in_phone_number(text):
     re_phone = r'(?:\+?\d?-?\(?\d{3}\)?[\s\-\.]?)?\d{3}[\s\-\.]?\d{4}'
     for number in re.finditer(re_phone, text):
@@ -12,26 +32,7 @@ def replace_hyphen_on_shortdash_in_phone_number(text):
 
 
 def typograph(text):
-    patterns = [
-        # remove windows line breaks
-        (r'\n\r|\r\n', r'\n'),
-        # remove redundant spaces and line breaks
-        (r'([\s])\1+', r'\1'),
-        # remove redundant line breaks at start of line
-        (r'^[\n ]+([^\n ])', r'\1'),
-        # replace quotes
-        (r"""(['"])(.*)(\1)""", r'«\2»'),
-        # replace hyphen on dash after punctuation
-        (r'(\.{3}\s*|[!?\.,]\s*|^\s*)(-)', r'\1—'),
-        # replace hyphen on dash between two words
-        (r'([\w]\s+)(-)(\s+[\w])', r'\1—\3'),
-        # binding of numbers followed by the words non-breaking hyphen
-        (r'(\d)([^\w]|_)+([^\W\d_])', r'\1&#8209;\3'),
-        # binding short words followed by the words
-        (r'(\b[^\W\d]{1,2})\s+\b', r'\1&nbsp;'),
-    ]
-
-    for pattern, repl in patterns:
+    for pattern, repl in PATTERNS:
         text = re.sub(pattern, repl, text, flags=re.MULTILINE)
     text = replace_hyphen_on_shortdash_in_phone_number(text)
     return text
